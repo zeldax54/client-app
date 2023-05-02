9f4bc47563a837298d5f12953cde310858961828
Hector Luis <hector.duran@tarmac.io>
2023-05-02 00:20:28 -0400

    add PhoneBook start

---
+++ src/Models/BussinessModel/PhonePrefix.ts
@@ -0,0 +1,6 @@
+export interface PhonePrefix {
+   CountryCode?:string;
+   Prefix? : string ;
+   CountryName? : string ;
+
+}
--- src/app/phonebook/phonebook.component.ts
+++ src/app/phonebook/phonebook.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Table } from 'primeng/table';
 import { CountryModel } from 'src/Models/BussinessModel/CountryModel';
+import { ZoneModel } from 'src/Models/BussinessModel/ZoneModel';
+
 import { PhoneBookModel } from 'src/Models/BussinessModel/PhoneBookModel';
 import { HelperService } from '../helpers/services/helper.service';
+import { ZonesService } from '../zones/zones.service';
+import { PhonePrefix } from 'src/Models/BussinessModel/PhonePrefix';
 
 @Component({
   selector: 'app-phonebook',
@@ -10,8 +14,6 @@ import { HelperService } from '../helpers/services/helper.service';
   styleUrls: ['./phonebook.component.scss'],
 })
 export class PhonebookComponent implements OnInit {
-
-  constructor( private helperService: HelperService){}
 
   countries:CountryModel[]=[];
   headerTile: string = '';
@@ -25,14 +27,28 @@ export class PhonebookComponent implements OnInit {
   deletePhoneBooksDialog: boolean = false;
   selectedMulti: any[] = [];
   selectedCountry:CountryModel={};
+  zones:ZoneModel[]=[];
+  selectedZone:ZoneModel={};
+  prefixs : PhonePrefix[]=[];
+  selectedPrefix:PhonePrefix={};
+
+  constructor( private helperService: HelperService,private zonesService:ZonesService){}
 
 
   ngOnInit(): void {
 
+    this.headerTile = $localize`${'Add a person to your PhoneBook'}`;
+
      this.helperService.countries().pipe().subscribe(response=>{
       this.countries = response;
-     })
-    this.cols = [
+      this.prefixs = response.map(c => ({ CountryCode : c.Code, Prefix : c.PhonePrefix,CountryName : c.Name }));
+     });
+
+     this.zonesService.getZones().subscribe(response=>{
+      this.zones = response;
+     });
+
+     this.cols = [
       { field: 'Name', header: $localize`${'Name'}` },
       { field: 'LastName', header: $localize`${'LastName'}` },
       { field: 'Country', header: $localize`${'Country'}` },
